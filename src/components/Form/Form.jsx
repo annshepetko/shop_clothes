487cfbbb12d9a18db1828099253d238b43a69d72
AnShepetko <111201419+AnShepetko@users.noreply.github.com>
2023-02-01 23:26:54 +0200

    form done.

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -27,23 +27,34 @@ export default function Form({orderItems = []}) {
         </div>
         <form className={styles.order_inputs}>
             <div className={styles.order_inputs_text}>
-                <label for='userName' > Enter your name </label>
+                <h2 className={styles.order_inputs_title} >Enter your details</h2>
+                <label for='userName' > Your name </label>
                 <input id='userName'type="text" />
-                <label for='userName' > Enter your name </label>
-                <input type="text" />
-                <label for='userName' > Enter your name </label>
-                <input type="text" />
-                <label for='userName' > Enter your name </label>
-                <input type="text" />
-                <label for='userName' > Enter your name </label>
-                <input type="text" />
-                <label for='userName' > Enter your name </label>
-                <input type="text" />
+                <label for='surname' >  Your surname </label>
+                <input type="text" id='surname'/>
+                <label for='patronym' > Your patronym </label>
+                <input type="text" id='patronym' />
+                <label for='city' > Your city/town </label>
+                <input type="text" id='city' />
+                <label for='number' >  Your phone number </label>
+                <input type="tel"id='number' />
+                <label for='gmail' >  Your gmail </label>
+                <input type="gmail" id='gmail' />
             </div>
             <div className={styles.order_inputs_radio}>
-                <input type="radio" name="" id="" />
-                <input type="radio" name="" id="" />
-                <input type="radio" name="" id="" />
+                <h2  className={styles.order_inputs_title}>Select a delivery method</h2>
+                <div className={styles.order_inputs_radio_block}>
+                    <input type="radio" name= "1" id="NovaPoshta" />
+                    <label htmlFor="NovaPoshta">Nova poshta</label>
+                </div>
+                <div className={styles.order_inputs_radio_block}>
+                    <input type="radio" name="1" id="meestExpress" />
+                    <label htmlFor="meestExpress">Meest Express</label>
+                </div>
+                <div className={styles.order_inputs_radio_block}>
+                    <input type="radio" name="1" id="Ukrposhta" />
+                    <label htmlFor="Ukrposhta">Ukr poshta</label>
+                </div>
             </div>
         </form>
     </div>
